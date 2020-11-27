a5a2fd838637c3613d022c4b7c8714a1ad7e5339
Diego Raymundo <diegor_137@outlook.es>
2020-11-26 20:56:36 -0600

    Rest Server - usuarios

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,22 @@
 //======
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+//======
+//ENTORNO
+//======
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//======
+//BASE DE DATOS
+//======
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://dieggo:kfJgWLrE32h3uC71@cluster0.zpil3.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
