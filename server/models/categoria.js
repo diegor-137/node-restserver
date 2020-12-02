805c7ac45bd14e24778a17e6e7601c24651ab1bf
Diego Raymundo <diegor_137@outlook.es>
2020-12-02 17:47:21 -0600

    Productos y categorias

---
+++ server/models/categoria.js
@@ -0,0 +1,20 @@
+const moongose = require('mongoose');
+
+
+
+let Schema = moongose.Schema;
+
+let categoriaSchema = new Schema({
+
+    descripcion: {
+        type: String,
+        unique: true,
+        required: [true, 'La descripcion es obligatoria']
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario'
+    }
+});
+
+module.exports = moongose.model('Categoria', categoriaSchema);
